a6455c97c640e4ee3191addd81cdf8558b6eb286
Anglammaroth <48485796+Anglammaroth@users.noreply.github.com>
2019-04-12 00:53:26 -0400

    Add files via upload

---
+++ Ridenour_Lab_8/src/ridenour_lab_8/Ridenour_Lab_8.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ridenour_lab_8;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Ridenour_Lab_8 extends Application
+{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage ps) 
+    {
+        
+    }
+    
+    private void format()
+    {
+        
+    }
+    
+    private void title()
+    {
+        
+    }
+    
+    private void stage1()
+    {
+        
+    }
+    
+    private void stage2()
+    {
+        
+    }
+    
+    private void stage3()
+    {
+        
+    }
+
+
+
+
+}
